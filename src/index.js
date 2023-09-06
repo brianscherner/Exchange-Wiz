338573aa5acb6082152da69e7021b0751bfcbe50
Brian Scherner <brianscherner@gmail.com>
2023-09-06 13:29:46 -0700

    Updat convertCurrency function to display results in DOM. Add abbreviation for US currency to index.html.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ async function getCurrencyConversionRates() {
   let response = await CurrencyService.getCurrencyConversionRates();
   console.log("Response: ", response);
   if (response[0].result === "success") {
-    return convertCurrency(response);
+    return convertCurrencyAndDisplayInDom(response);
   }
 }
 
@@ -15,39 +15,30 @@ async function getCurrencyConversionRates() {
 //   document.querySelector("showCurrencyConversion").innerText = `There was an error accessing the currency data for ${error}: `;
 // }
 
-function convertCurrency(response) {
+function convertCurrencyAndDisplayInDom(response) {
   const convertedEuro = document.querySelector("#usDollars").value * response[0].conversion_rates.EUR;
   const convertedJapaneseYen = document.querySelector("#usDollars").value * response[0].conversion_rates.JPY;
   const convertedSwissFranc = document.querySelector("#usDollars").value * response[0].conversion_rates.CHF;
   const convertedAustralianDollar = document.querySelector("#usDollars").value * response[0].conversion_rates.AUD;
   const convertedHongKongDollar = document.querySelector("#usDollars").value * response[0].conversion_rates.HKD;
   const currencyType = document.querySelector("input[name='currency']:checked").value;
+  let americanDollars = document.querySelector("#usDollars").value;
   if (currencyType === "euro") {
-    console.log("Converted Euro: ", convertedEuro);
-    return convertedEuro;
+    document.querySelector("p#showConvertedCurrency").innerText = `${americanDollars} US Dollars equals ${convertedEuro} Euros.`;
   } else if (currencyType === "yen") {
-    console.log("Converted Yen: ", convertedJapaneseYen);
-    return convertedJapaneseYen;
+    document.querySelector("p#showConvertedCurrency").innerText = `${americanDollars} US Dollars equals ${convertedJapaneseYen} Japanese Yen.`;
   } else if (currencyType === "franc") {
-    console.log("Converted Franc: ", convertedSwissFranc);
-    return convertedSwissFranc;
+    document.querySelector("p#showConvertedCurrency").innerText = `${americanDollars} US Dollars equals ${convertedSwissFranc} Swiss Francs.`;
   } else if (currencyType === "australian") {
-    console.log("Converted Aussie Dollar: ", convertedAustralianDollar);
-    return convertedHongKongDollar;
+    document.querySelector("p#showConvertedCurrency").innerText = `${americanDollars} US Dollars equals ${convertedAustralianDollar} Australian Dollars.`;
   } else if (currencyType === "hongKong") {
-    console.log("Converted HK dollar: ", convertedHongKongDollar);
-    return convertedHongKongDollar;
+    document.querySelector("p#showConvertedCurrency").innerText = `${americanDollars} US Dollars equals ${convertedHongKongDollar} Hong Kong Dollars.`;
   }
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const usDollars = document.querySelector("#usDollars").value;
-  const currencyToConvert = document.querySelector("input[name='currency']:checked").value;
-  console.log(usDollars);
-  console.log(currencyToConvert);
   getCurrencyConversionRates();
-  // convertCurrency(response);
 }
 
 window.addEventListener("load", function() {
