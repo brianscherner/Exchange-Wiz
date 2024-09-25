5d0c641501ffd0671eb22e5d161fdc92070eac0c
Brian Scherner <brianscherner@gmail.com>
2024-09-25 16:58:55 -0700

    Remove global variables and improve documentation.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import './css/styles.css';
 import { callToServerlessFunction } from './services/serverless-function-call.js';
 import { currencyNames } from './services/currency-names.js';
 
-// global variables containing API data and currency names
-let currencyServiceApiData = null;
-const currencyNamesArray = currencyNames;
-
 async function fetchCurrencyConversionRates() {
-  // awaits API response - if call is successful it obtains currency conversion rates and the names of each one
+  // awaits API response - if call is successful it obtains all currency conversion rates and their names.
 
   // if unsuccessful, returns an error
+
   const response = await callToServerlessFunction();
-  currencyServiceApiData = response;
   if (response.result === "success") {
-    getCurrencyDataAndAddCurrencyNamesToForm();
+    getCurrencyDataAndAddCurrencyNamesToForm(response.conversion_rates);
   } else {
     printError(response);
   }
@@ -26,25 +22,23 @@ function printError(error) {
   document.querySelector("p#showConvertedCurrency").innerText = `There was an error accessing currency exchange data: ${error}.`;
 }
 
-function getCurrencyDataAndAddCurrencyNamesToForm() {
-  // gets conversion rates from API
-  let currencyExchangeRates = currencyServiceApiData.conversion_rates;
-
-  // extracts 3 digit currency code for each currency
-  const currencyCodes = Object.keys(currencyExchangeRates);
+function getCurrencyDataAndAddCurrencyNamesToForm(apiResponse) {
+  // gets conversion rates from API extracts 3 digit currency code for each currency
+  const currencyCodes = Object.keys(apiResponse);
   currencyCodes.splice(128, 1);
 
   // extracts exchange rate for each currency
-  const currencyCodeValues = Object.values(currencyExchangeRates);
+  const currencyCodeValues = Object.values(apiResponse);
   currencyCodeValues.splice(128, 1);
 
+  // user selects a currency
   let currencySelection = document.getElementById("currencySelection");
 
   // loops through all currency codes and adds the name of each currency next to the 3 digit code
 
-  // dynamically appends each one to the dropdown menu with its name, value, and id
+  // dynamically appends each currency to the dropdown menu with its name, code, value, and an id
   currencyCodes.forEach((code, index) => {
-    const name = currencyNamesArray[index];
+    const name = currencyNames[index];
     let currency = `${name} (${code})`;
     let currencyElement = document.createElement("option");
     currencyElement.text = currency;
@@ -54,13 +48,16 @@ function getCurrencyDataAndAddCurrencyNamesToForm() {
     currencySelection.appendChild(currencyElement);
   });
 
+  // set base currency (USD) to be invisible, as a user likely won't want or need to compare USD to USD
   currencySelection.options[1].style.display = "none";
 }
 
 function calculateCurrencyConversion() {
-  // gets currency selection and entered value in USD
+  // gets currency selection
   let currencySelection = document.getElementById("currencySelection");
+  // gets inputted USD amount
   let usDollarAmount = parseFloat(document.getElementById("usDollars").value);
+  // gets value of selected currency
   let selectedCurrencyValue = parseFloat(currencySelection.value);
 
   // accesses currency name and code and splits code from the name
@@ -71,14 +68,19 @@ function calculateCurrencyConversion() {
   // calculates the currency conversion
   let currencyConversionResult = parseFloat((usDollarAmount * selectedCurrencyValue).toFixed(4));
 
-  // displays the result
+  // displays the result - the value with the 3 digit code
   document.getElementById("showConvertedCurrency").innerText = `${currencyConversionResult.toLocaleString('en-US', { minimumFractionDigits: 4 })} ${isolatedCurrencyCode}`;
 }
 
 window.addEventListener("load", function() {
-  // loads function that gets API data from the function that calls the serverless function, which calls the API
+  // loads function that gets API data from serverless-function-call.js
+
+  // serverless-function-call.js calls Netlify function for API data
+
+  // Netlify function calls ExchangeRate-API for response
   fetchCurrencyConversionRates();
   document.getElementById("conversionForm").addEventListener("submit", function(event) {
+    // when form is submitted, the hidden paragraph is revealed with the calculated result
     event.preventDefault();
     document.getElementById("showConvertedCurrency").removeAttribute("class", "hidden");
     calculateCurrencyConversion();
