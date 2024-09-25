99e44de9036b2c337482178b5868540965b4d0a3
Brian Scherner <brianscherner@gmail.com>
2024-09-25 15:24:59 -0700

    Remove console logs.

--- src/services/serverless-function-call.js
+++ src/services/serverless-function-call.js
@@ -2,13 +2,10 @@
 
 export const callToServerlessFunction = async () => {
   const apiCall = window.location.hostname === "localhost" ? "http://localhost:8888/.netlify/functions/api-call" : "/.netlify/functions/api-call";
-  console.log('Frontend call: ', apiCall);
 
   try {
     const fetchedApiData = await fetch(apiCall);
-    console.log("Fetched data: ", fetchedApiData);
     const apiDataJson = await fetchedApiData.json();
-    console.log("JSON obj: ", apiDataJson);
 
     if (!fetchedApiData.ok) {
       const errorMessage = `${apiDataJson.status} ${apiDataJson.statusText}`;
@@ -20,5 +17,4 @@ export const callToServerlessFunction = async () => {
   } catch (error) {
     return error;
   }
-
 };
